66a1953289a78acd5f293dafafc9cf3fe296cb2c
Сергей <lockup86@rambler.ru>
2025-09-02 13:48:24 +0300

    Начало

---
+++ src/main/java/com/example/SoapRestApi/SoapRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.SoapRestApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapRestApiApplication.class, args);
+	}
+
+}
